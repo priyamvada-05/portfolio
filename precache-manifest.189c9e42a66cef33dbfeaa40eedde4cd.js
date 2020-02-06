343a81793bf041eaddc2c45aad585687a4315609
priyamvada <pranshulpandey5@gmail.com>
2020-02-06 20:12:51 +0000

    Updates

---
+++ precache-manifest.189c9e42a66cef33dbfeaa40eedde4cd.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6695c99693e7960ff4f190ffe529abf6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "79d095b2982247ceaf0c",
+    "url": "/portfolio/static/css/2.145e489a.chunk.css"
+  },
+  {
+    "revision": "09bf399052cda7c0cd06",
+    "url": "/portfolio/static/css/main.f4604c29.chunk.css"
+  },
+  {
+    "revision": "79d095b2982247ceaf0c",
+    "url": "/portfolio/static/js/2.fc74df81.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.fc74df81.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09bf399052cda7c0cd06",
+    "url": "/portfolio/static/js/main.c4f19339.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "8d0e4f1ef231aa7ea74690b29718f997",
+    "url": "/portfolio/static/media/Capture1.8d0e4f1e.PNG"
+  },
+  {
+    "revision": "9e6c7295b15e8a4d032db83f32c41a63",
+    "url": "/portfolio/static/media/Capture2.9e6c7295.PNG"
+  },
+  {
+    "revision": "490041e89658977f45989728f57f8770",
+    "url": "/portfolio/static/media/Capture3.490041e8.PNG"
+  },
+  {
+    "revision": "2cc640d8cfe242076892eaf30fd3b47f",
+    "url": "/portfolio/static/media/Capture4.2cc640d8.PNG"
+  },
+  {
+    "revision": "075ea82c42b8abc71e55c4646862c420",
+    "url": "/portfolio/static/media/Capture5.075ea82c.PNG"
+  },
+  {
+    "revision": "c839dc03e5d17d3cd61dbf23ae3ca88b",
+    "url": "/portfolio/static/media/R.c839dc03.png"
+  },
+  {
+    "revision": "c00074d94fe2e5d7303b2d44a182351b",
+    "url": "/portfolio/static/media/gmail.c00074d9.png"
+  },
+  {
+    "revision": "1b8934ce7f7ab9d54b8199f54babb928",
+    "url": "/portfolio/static/media/img.1b8934ce.png"
+  },
+  {
+    "revision": "c40f2298bf663ff67f5f72f934e33d38",
+    "url": "/portfolio/static/media/img.c40f2298.jpeg"
+  },
+  {
+    "revision": "3f2ea8b6e94c0ee318e931e9558ab836",
+    "url": "/portfolio/static/media/linkedin.3f2ea8b6.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
